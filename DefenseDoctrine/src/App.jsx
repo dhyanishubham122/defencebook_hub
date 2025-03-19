bd617e7258ba9ab0f10be99635263ad00bc6fce4
Shubham <dhyanishubham122@gmail.com>
2025-03-19 18:58:38 +0530

    changes

--- DefenseDoctrine/src/App.jsx
+++ DefenseDoctrine/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Route path="/*" element={<UserRouter />} />
 
       {/* Admin Routes */}
-      <Route path="/admin/*" element={<AdminRouter />} />
+      <Route path="admin/*" element={<AdminRouter />} />
     </Routes>
   );
 }
